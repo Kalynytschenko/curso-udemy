befebba2c247b70b630b7842c004cac7970ac880
Alexandre <alexandre.kalynytschenko@hotmail.com>
2021-04-14 08:52:48 -0300

    aulas de encapsulamento

---
+++ Encapsulameto_construtores_modificadores/ContaBancaria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulameto_construtores_modificadores
+{
+    class ContaBancaria
+    {
+        public int NumConta { get; private set; }
+
+        public string Titular { get; set; }
+
+        public int Saldo { get; set; }
+
+
+        public int Deposito(int valor)
+        {
+            Saldo = Saldo + valor;
+            return Saldo;
+        }
+
+        public int Saque(int valor)
+        {
+            Saldo = Saldo - valor -5;
+            return Saldo;
+        }
+
+
+    }
+}
---
+++ Encapsulameto_construtores_modificadores/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Encapsulameto_construtores_modificadores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool depIni = false;
+            char depInicial;
+            int valor = 0;
+
+            ContaBancaria contaBancaria = new ContaBancaria();
+
+            Console.WriteLine("Entre com o numero da conta:");
+            contaBancaria.NumConta = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o Titularrr da conta:");
+            contaBancaria.Titular = Console.ReadLine();
+            Console.WriteLine("Havera deposito inicial (s/n)?");
+            depInicial = char.Parse((Console.ReadLine()));
+
+            if (depInicial == 'S' || depInicial == 's')
+            {
+                Console.WriteLine("Entre com o valor de deposito inincial:");
+                valor = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Dados da Conta:\nConta " + contaBancaria.NumConta + ", Titular: " + contaBancaria.Titular + ", Saldo: $" + contaBancaria.Deposito(valor));
+
+                Console.WriteLine("Entre com o valor de deposito:");
+                valor = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Dados da Conta atualizados:\nConta " + contaBancaria.NumConta + ", Titular: " + contaBancaria.Titular + ", Saldo: $" + contaBancaria.Deposito(valor));
+
+                Console.WriteLine("Entre com o valor para saque:");
+                valor = int.Parse(Console.ReadLine());
+                Console.WriteLine("Dados da Conta atualizados:\nConta " + contaBancaria.NumConta + ", Titular: " + contaBancaria.Titular + ", Saldo: $" + contaBancaria.Saque(valor));
+            }
+            else
+            {
+                Console.WriteLine("Dados da Conta:\nConta " + contaBancaria.NumConta + ", Titular: " + contaBancaria.Titular + ", Saldo: $" + contaBancaria.Deposito(valor));
+
+                Console.WriteLine("Entre com o valor de deposito:");
+                valor = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Dados da Conta atualizados:\nConta " + contaBancaria.NumConta + ", Titular: " + contaBancaria.Titular + ", Saldo: $" + contaBancaria.Deposito(valor));
+
+                Console.WriteLine("Entre com o valor para saque:");
+                valor = int.Parse(Console.ReadLine());
+                Console.WriteLine("Dados da Conta atualizados:\nConta " + contaBancaria.NumConta + ", Titular: " + contaBancaria.Titular + ", Saldo: $" + contaBancaria.Saque(valor));
+            }
+
+
+
+
+
+
+
+        }
+    }
+}
